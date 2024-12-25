6ea96e1d84995c9f54e133ff24d4010aae4d5f80
Oussama riadi <Oussamariadi00000@gmail.com>
2024-12-25 21:57:56 +0100

    This is from Riadi

---
+++ test/read.kt
@@ -0,0 +1,7 @@
+package com.example.restov1
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
