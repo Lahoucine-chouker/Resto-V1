d98112a96792a2982b43249d06a29cd08421dcc2
Oussama riadi <71236386+OussamaRiadi@users.noreply.github.com>
2024-12-26 12:22:11 +0100

    Delete test directory

--- test/read.kt
+++
@@ -1,7 +0,0 @@
-package com.example.restov1
-
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
